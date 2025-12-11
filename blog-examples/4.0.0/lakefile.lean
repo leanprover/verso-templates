808545dac0498e6b07167816f9cca9a8a63907ca
David Thrane Christiansen <david@davidchristiansen.dk>
2025-12-11 22:38:54 +0100

    chore: update to 4.26.0-rc2 (#6)

--- blog-examples/4.0.0/lakefile.lean
+++ blog-examples/4.0.0/lakefile.lean
@@ -2,8 +2,12 @@ import Lake
 
 open Lake DSL
 
-require subverso from git "https://github.com/leanprover/subverso.git"@"main"
+require subverso from git "https://github.com/leanprover/subverso.git"@"no-modules/1d663ae67d8088111bb37c3c526fe2d43e4df01c"
 
-package examples
+
+
+package examples where
+  buildDir := ".lake/build"
+  packagesDir := ".lake/packages"
 
 lean_lib Old
